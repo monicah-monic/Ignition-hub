ac63142ca544a3fc486b0fd66261441e85d705f1
monicah-monic <monicahwanjiru205@gmail.com>
2025-05-27 18:12:36 +0300

    create db

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,22 @@
 from datetime import datetime
 from sqlalchemy import create_engine, Column, String, Integer,ForeignKey
-from sqlalchemy.orm import sessionmaker, declartive_base, relationship
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
-Base = declartive_base()
+Base = declarative_base()
+
+engine = create_engine('sqlite:///lib/ignition.db')
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind= engine)
+session = Session()
+
+
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer(), primary_key = True)
+    name =Column(String())
+    email = Column(String())
+    contact = Column(Integer())
+
+    
